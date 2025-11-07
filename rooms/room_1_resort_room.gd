fdc0c72b0a6c3d2aaeed3004d9610e41624a433a
Greg Smith <variablegreg@gmail.com>
2025-11-06 16:40:51 -0800

    Working on dialogue box hell right now

--- rooms/room_1_resort_room.gd
+++ rooms/room_1_resort_room.gd
@@ -1,13 +1,27 @@
 extends Node2D
 
+@onready var text_box = $TextBox
 
 var first_visit: bool = true
 
 func _ready():
+	text_box.hide()
+	
 	if first_visit:
 		show_opening_text()
 		first_visit = false
+	
+	text_box.text_finished.connect(_on_text_finished)
 
 func show_opening_text():
-	# We'll add a dialogue box here next
-	print("Opening exposition text would appear here")
+	var opening_text = "You wake up with a pounding headache. Last night was... intense. The Spring Fling Cruise party at the resort got out of hand. Your cruise ship leaves in a few hours, and you need to find your locker key to get your stuff and get out of here."
+	text_box.show_text(opening_text)
+
+func _on_text_finished():
+	# Text box closed, player can now interact with room
+	enable_hotspots()
+	print("Player can now interact with the room")
+
+func enable_hotspots():
+	# We'll implement this when we add clickable hotspots
+	pass
--- ui/text_box.gd
+++ ui/text_box.gd
@@ -3,11 +3,52 @@ extends Control
 signal text_finished
 
 @onready var text_label = $DialoguePanel/TextLabel
+@onready var continue_button = $DialoguePanel/ContinueButton
+
+var full_text: String = ""
+var current_char: int = 0
+var typing_speed: float = 0.05  # Seconds per character
+var typing: bool = false
+
+func _ready():
+	if continue_button:
+		continue_button.hide()
 
 func show_text(text: String):
-	text_label.text = text
+	full_text = text
+	current_char = 0
+	text_label.text = ""
 	show()
+	if continue_button:
+		continue_button.hide()
+	typing = true
+	start_typing()
+
+func start_typing():
+	while current_char < full_text.length() and typing:
+		text_label.text += full_text[current_char]
+		current_char += 1
+		
+		# Auto-scroll to bottom
+		await get_tree().create_timer(typing_speed).timeout
+		text_label.scroll_to_line(text_label.get_line_count())
+	
+	# Text finished typing
+	typing = false
+	if continue_button:
+		continue_button.show()
 
 func _on_continue_button_pressed():
 	text_finished.emit()
 	hide()
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and typing:
+		if current_char < full_text.length():
+			# Skip to end
+			text_label.text = full_text
+			current_char = full_text.length()
+			typing = false
+			text_label.scroll_to_line(text_label.get_line_count())
+			if continue_button:
+				continue_button.show()
