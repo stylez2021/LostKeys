5368fbaf59e2f046e492966025277868c7969aa5
Greg Smith <variablegreg@gmail.com>
2025-12-11 09:42:55 -0800

    Hotspot and zoom in troubleshooting

--- rooms/dresser_zoom.gd
+++ rooms/dresser_zoom.gd
@@ -2,19 +2,26 @@ extends CanvasLayer
 
 signal zoom_closed
 
+@onready var flyer_hotspot = $FlyerHotspot
+
 func _ready():
 	hide()  # Start hidden
 		
 func show_zoom():
 	show()
+	if flyer_hotspot:
+		flyer_hotspot.monitoring = true  # Enable the hotspot
 	print("DresserZoom showing!")
 
 func _on_back_button_pressed():
 	zoom_closed.emit()
+	if flyer_hotspot:
+		flyer_hotspot.monitoring = false  # Disable the hotspot
 	hide()
 
 func _on_flyer_hotspot_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("Flyer hotspot clicked in zoom!")  # Debug
 		on_flyer_clicked()
 
 func on_flyer_clicked():
@@ -26,8 +33,8 @@ func on_flyer_clicked():
 		GameManager.add_item("flyer")
 		print("Picked up flyer!")
 		
-		# Hide the flyer hotspot
-		$FlyerHotspot.queue_free()
+		if flyer_hotspot:
+			flyer_hotspot.queue_free()
 		
 		# Close the zoom view
 		zoom_closed.emit()
